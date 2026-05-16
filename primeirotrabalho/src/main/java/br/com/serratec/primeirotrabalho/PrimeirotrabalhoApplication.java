bf2372451b2f106002135d2ad5d4e0a544aca912
lc-lira <lbrancolira@gmail.com>
2026-05-15 21:27:57 -0300

    Criacao dos packages e configuraçao do h2

---
+++ primeirotrabalho/src/main/java/br/com/serratec/primeirotrabalho/PrimeirotrabalhoApplication.java
@@ -0,0 +1,13 @@
+package br.com.serratec.primeirotrabalho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeirotrabalhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeirotrabalhoApplication.class, args);
+	}
+
+}
