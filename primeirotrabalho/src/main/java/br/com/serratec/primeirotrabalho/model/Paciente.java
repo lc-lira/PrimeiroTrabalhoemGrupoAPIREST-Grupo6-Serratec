cde9bb51b58fe72e572025b9102ba89b07225e0e
gabrielamontescoelho <gabrielamontescoelho@gmail.com>
2026-05-18 23:04:39 -0300

    versão final do trabalho

--- primeirotrabalho/src/main/java/br/com/serratec/primeirotrabalho/model/Paciente.java
+++ primeirotrabalho/src/main/java/br/com/serratec/primeirotrabalho/model/Paciente.java
@@ -1,6 +1,11 @@
 package br.com.serratec.primeirotrabalho.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -13,4 +18,17 @@ public class Paciente extends Pessoa {
     public Paciente(Long codigo, String nome, String email, String telefone) {
         super(codigo, nome, email, telefone);
     }
+
+    @JsonManagedReference
+    @OneToMany(mappedBy = "paciente")
+    private List<Consulta> consultas;
+
+    public List<Consulta> getConsultas() {
+        return consultas;
+    }
+
+    public void setConsultas(List<Consulta> consultas) {
+        this.consultas = consultas;
+    }
+
 }
